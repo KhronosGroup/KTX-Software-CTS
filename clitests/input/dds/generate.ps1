4ca3309076019d437c1ce0b3184684d53f9bd14a
Walid Chtioui <walid.chtioui.main@gmail.com>
2026-07-29 10:11:38 +0200

    remove large DDS testfiles and golden files
    Signed-off-by: Walid Chtioui <walid.chtioui.main@gmail.com>

--- clitests/input/dds/generate.ps1
+++ clitests/input/dds/generate.ps1
@@ -22,7 +22,7 @@
 $ErrorActionPreference="Stop"
 
 # Path to CTS inputs from KTX-Software
-$INPUT_DIR="KTX-Software\tests\cts\clitests\input"
+$INPUT_DIR="../input"
 
 # Compute max number of mip levels for given dimensions
 # [Math]::Log2 doesn't work hence why the division below
@@ -60,8 +60,8 @@ texconv -nologo -y -m 1 -f BC4_UNORM -ft dds ${INPUT_DIR}\png\r8_unorm_bc4.png -
 texconv -nologo -y -m 1 -f BC4_SNORM -ft dds ${INPUT_DIR}\png\r8_unorm_bc4.png -o .; Move-Item -Force -Path r8_unorm_bc4.dds -Destination valid_compressed_BC4_SNORM_40x40.dds
 texconv -nologo -y -m 1 -f BC5_UNORM -ft dds ${INPUT_DIR}\png\rg8_unorm_bc5.png -o .; Move-Item -Force -Path rg8_unorm_bc5.dds -Destination valid_compressed_BC5_UNORM_40x40.dds
 texconv -nologo -y -m 1 -f BC5_SNORM -ft dds ${INPUT_DIR}\png\rg8_unorm_bc5.png -o .; Move-Item -Force -Path rg8_unorm_bc5.dds -Destination valid_compressed_BC5_SNORM_40x40.dds
-texconv -nologo -y -m 1 -f BC6H_UF16 -ft dds ${INPUT_DIR}\png\basic_RGB16_16x16.png -o .; Move-Item -Force -Path basic_RGB16_16x16.dds -Destination valid_compressed_BC6H_UFLOAT_16x16.dds
-texconv -nologo -y -m 1 -f BC6H_SF16 -ft dds ${INPUT_DIR}\png\sbit16_RGB16_16x16.png -o .; Move-Item -Force -Path sbit16_RGB16_16x16.dds -Destination valid_compressed_BC6H_SFLOAT_16x16.dds
+texconv -nogpu -nologo -y -m 1 -f BC6H_UF16 -ft dds ${INPUT_DIR}\png\basic_RGB16_16x16.png -o .; Move-Item -Force -Path basic_RGB16_16x16.dds -Destination valid_compressed_BC6H_UFLOAT_16x16.dds
+texconv -nogpu -nologo -y -m 1 -f BC6H_SF16 -ft dds ${INPUT_DIR}\png\sbit16_RGB16_16x16.png -o .; Move-Item -Force -Path sbit16_RGB16_16x16.dds -Destination valid_compressed_BC6H_SFLOAT_16x16.dds
 texconv -nologo -y -m 1 -f BC7_UNORM -ft dds ${INPUT_DIR}\png\rgba8_unorm_bc7.png -o .; Move-Item -Force -Path rgba8_unorm_bc7.dds -Destination valid_compressed_BC7_UNORM_40x40.dds
 texconv -nologo -y -m 1 -f BC7_UNORM_SRGB -ft dds ${INPUT_DIR}\png\rgba8_srgb_bc7.png -o .; Move-Item -Force -Path rgba8_srgb_bc7.dds -Destination valid_compressed_BC7_SRGB_40x40.dds
 
@@ -76,17 +76,14 @@ texconv -nologo -y -m 1 -f Y416 -ft dds ${INPUT_DIR}\png\basic_RGBA16_16x16.png 
 texconv -nologo -y -m 1 -f YUY2 -ft dds ${INPUT_DIR}\png\basic_RGBA16_16x16.png -o .; Move-Item -Force -Path basic_RGBA16_16x16.dds -Destination unsupported_YUY2_16x16.dds
 texconv -nologo -y -m 1 -f A4B4G4R4_UNORM -ft dds ${INPUT_DIR}\png\basic_RGBA16_16x16.png -o .; Move-Item -Force -Path basic_RGBA16_16x16.dds -Destination unsupported_A4B4G4R4_UNORM_16x16.dds
 
-# DDS volume textures
-texassemble volume -nologo -y -f R8G8B8A8_UNORM_SRGB -o valid_R8G8B8A8_SRGB_slices_6_128x128.dds `
-  ${INPUT_DIR}\png\slice0.png ${INPUT_DIR}\png\slice1.png ${INPUT_DIR}\png\slice2.png `
-  ${INPUT_DIR}\png\slice3.png ${INPUT_DIR}\png\slice4.png ${INPUT_DIR}\png\slice5.png
-
-# DDS cubemap textures
-texassemble cube -nologo -y -f R8G8B8A8_UNORM_SRGB -o valid_R8G8B8A8_SRGB_faces_6_128x128.dds `
-  ${INPUT_DIR}\png\slice0.png ${INPUT_DIR}\png\slice1.png ${INPUT_DIR}\png\slice2.png `
-  ${INPUT_DIR}\png\slice3.png ${INPUT_DIR}\png\slice4.png ${INPUT_DIR}\png\slice5.png
-
-# DDS array textures
-texassemble array -nologo -y -f R8G8B8A8_UNORM_SRGB -o valid_R8G8B8A8_SRGB_layers_6_128x128.dds `
-  ${INPUT_DIR}\png\slice0.png ${INPUT_DIR}\png\slice1.png ${INPUT_DIR}\png\slice2.png `
-  ${INPUT_DIR}\png\slice3.png ${INPUT_DIR}\png\slice4.png ${INPUT_DIR}\png\slice5.png
+# DDS volume, cubemap, and array textures
+# TODO: these are currently too large and I don't know yet how to assemble them using BC7 compression
+# texassemble volume -nologo -y -f R8G8B8A8_UNORM_SRGB -o valid_R8G8B8A8_SRGB_slices_6_128x128.dds `
+#   ${INPUT_DIR}\png\slice0.png ${INPUT_DIR}\png\slice1.png ${INPUT_DIR}\png\slice2.png `
+#   ${INPUT_DIR}\png\slice3.png ${INPUT_DIR}\png\slice4.png ${INPUT_DIR}\png\slice5.png
+# texassemble cube -nologo -y -f R8G8B8A8_UNORM_SRGB -o valid_R8G8B8A8_SRGB_faces_6_128x128.dds `
+#   ${INPUT_DIR}\png\slice0.png ${INPUT_DIR}\png\slice1.png ${INPUT_DIR}\png\slice2.png `
+#   ${INPUT_DIR}\png\slice3.png ${INPUT_DIR}\png\slice4.png ${INPUT_DIR}\png\slice5.png
+# texassemble array -nologo -y -f R8G8B8A8_UNORM_SRGB -o valid_R8G8B8A8_SRGB_layers_6_128x128.dds `
+#   ${INPUT_DIR}\png\slice0.png ${INPUT_DIR}\png\slice1.png ${INPUT_DIR}\png\slice2.png `
+#   ${INPUT_DIR}\png\slice3.png ${INPUT_DIR}\png\slice4.png ${INPUT_DIR}\png\slice5.png
